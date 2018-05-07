06467e95d973630ce969373f32e8e57aa851ea55
edgarcosta92 <cedgar@ethz.ch>
2018-05-07 21:37:30 +0200

    put some tought under flow completion time

--- contrib/traffic-generation/model/traffic-generation.cc
+++ contrib/traffic-generation/model/traffic-generation.cc
@@ -9,67 +9,62 @@
 
 NS_LOG_COMPONENT_DEFINE ("traffic-generation");
 
-
 namespace ns3 {
 
 void
-InstallSink(Ptr<Node> node, uint16_t sinkPort, uint32_t duration, std::string protocol)
-{
+InstallSink(Ptr<Node> node, uint16_t sinkPort, uint32_t duration, std::string protocol) {
   //create sink helper
-  PacketSinkHelper packetSinkHelper ("ns3::TcpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), sinkPort));
+  PacketSinkHelper packetSinkHelper("ns3::TcpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), sinkPort));
 
-  if (protocol == "UDP"){
-  	packetSinkHelper.SetAttribute("Protocol",StringValue("ns3::UdpSocketFactory"));
-	}
+  if (protocol == "UDP") {
+    packetSinkHelper.SetAttribute("Protocol", StringValue("ns3::UdpSocketFactory"));
+  }
 
   ApplicationContainer sinkApps = packetSinkHelper.Install(node);
 
-  sinkApps.Start (Seconds (0));
+  sinkApps.Start(Seconds(0));
   //Only schedule a stop it duration is bigger than 0 seconds
-  if (duration != 0){
-  	sinkApps.Stop (Seconds (duration));
+  if (duration != 0) {
+    sinkApps.Stop(Seconds(duration));
   }
 }
 
 Ptr<Socket>
-InstallSimpleSend(Ptr<Node> srcHost, Ptr<Node> dstHost, uint16_t sinkPort, DataRate dataRate, uint32_t numPackets, std::string protocol)
-{
+InstallSimpleSend(Ptr<Node> srcHost, Ptr<Node> dstHost, uint16_t sinkPort, DataRate dataRate, uint32_t numPackets,
+                  std::string protocol) {
   Ptr<Socket> ns3Socket;
   uint32_t num_packets = numPackets;
 
   //had to put an initial value
-  if (protocol == "TCP")
-  {
-    ns3Socket = Socket::CreateSocket (srcHost, TcpSocketFactory::GetTypeId ());
+  if (protocol == "TCP") {
+    ns3Socket = Socket::CreateSocket(srcHost, TcpSocketFactory::GetTypeId());
+  } else {
+    ns3Socket = Socket::CreateSocket(srcHost, UdpSocketFactory::GetTypeId());
   }
-  else
-	{
-    ns3Socket = Socket::CreateSocket (srcHost, UdpSocketFactory::GetTypeId ());
-	}
 
 
   Ipv4Address addr = GetNodeIp(dstHost);
 
-  Address sinkAddress (InetSocketAddress (addr, sinkPort));
+  Address sinkAddress(InetSocketAddress(addr, sinkPort));
 
-  Ptr<SimpleSend> app = CreateObject<SimpleSend> ();
-  app->Setup (ns3Socket, sinkAddress, 1440, num_packets, dataRate);
+  Ptr<SimpleSend> app = CreateObject<SimpleSend>();
+  app->Setup(ns3Socket, sinkAddress, 1440, num_packets, dataRate);
 
-  srcHost->AddApplication (app);
+  srcHost->AddApplication(app);
 
-  app->SetStartTime (Seconds (1.));
+  app->SetStartTime(Seconds(1.));
   //Stop not needed since the simulator stops when there are no more packets to send.
   //app->SetStopTime (Seconds (1000.));
   return ns3Socket;
 }
 
 void
-InstallOnOffSend(Ptr<Node> srcHost, Ptr<Node> dstHost, uint16_t dport, DataRate dataRate, uint32_t packet_size, uint64_t max_size, double startTime)
-{
+InstallOnOffSend(Ptr<Node> srcHost, Ptr<Node> dstHost, uint16_t dport, DataRate dataRate, uint32_t packet_size,
+                 uint64_t max_size, double startTime) {
   Ipv4Address addr = GetNodeIp(dstHost);
-  Address sinkAddress (InetSocketAddress (addr, dport));
+  Address sinkAddress(InetSocketAddress(addr, dport));
 
-  Ptr<OnOffApplication> onoff_sender =  CreateObject<OnOffApplication>();
+  Ptr<OnOffApplication> onoff_sender = CreateObject<OnOffApplication>();
 
   onoff_sender->SetAttribute("Protocol", TypeIdValue(TcpSocketFactory::GetTypeId()));
   onoff_sender->SetAttribute("Remote", AddressValue(sinkAddress));
@@ -85,19 +80,18 @@ InstallOnOffSend(Ptr<Node> srcHost, Ptr<Node> dstHost, uint16_t dport, DataRate 
 }
 
 void
-InstallRateSend(Ptr<Node> srcHost, Ptr<Node> dstHost, uint16_t dport, uint32_t n_packets, uint64_t max_size, double duration, double startTime)
-{
-    if (duration <= 0){
-        NS_LOG_DEBUG("Install Rate Send: \t Bulk Send Instead!");
-        InstallNormalBulkSend(srcHost,dstHost,dport,max_size,startTime);
-        return;
-    }
+InstallRateSend(Ptr<Node> srcHost, Ptr<Node> dstHost, uint16_t dport, uint32_t n_packets, uint64_t max_size,
+                double duration, double startTime) {
+  if (duration <= 0) {
+    NS_LOG_DEBUG("Install Rate Send: \t Bulk Send Instead!");
+    InstallNormalBulkSend(srcHost, dstHost, dport, max_size, startTime);
+    return;
+  }
 
   Ipv4Address addr = GetNodeIp(dstHost);
-  Address sinkAddress (InetSocketAddress (addr, dport));
+  Address sinkAddress(InetSocketAddress(addr, dport));
 
-  Ptr<RateSendApplication> rate_send_app =  CreateObject<RateSendApplication>();
-
+  Ptr<RateSendApplication> rate_send_app = CreateObject<RateSendApplication>();
 
   rate_send_app->SetAttribute("Protocol", TypeIdValue(TcpSocketFactory::GetTypeId()));
   rate_send_app->SetAttribute("Remote", AddressValue(sinkAddress));
@@ -106,6 +100,12 @@ InstallRateSend(Ptr<Node> srcHost, Ptr<Node> dstHost, uint16_t dport, uint32_t n
   double interval_duration;
 
   max_size = max_size - (n_packets * 54);
+
+  //real number of packets to send
+  //remove syn, ack, fin, ack
+  if (n_packets > 4) {
+    n_packets = n_packets - 4;
+  }
 
   double bytes_per_sec = double(max_size) / duration;
 
@@ -116,13 +116,13 @@ InstallRateSend(Ptr<Node> srcHost, Ptr<Node> dstHost, uint16_t dport, uint32_t n
   uint64_t bytes_per_period = uint64_t(avg_size_per_packet);
 
   NS_LOG_DEBUG("Flow Features:    " << "\tNumber Packets: " << n_packets
-                                << "\t Bytes To send: " <<  max_size
-                                << "\t Duration: " << duration);
+                                    << "\t Bytes To send: " << max_size
+                                    << "\t Duration: " << duration);
 
   NS_LOG_DEBUG("Install Rate Send: " << "\tBytes per sec: " << bytes_per_sec
-                                     << "\t Avg Pkt Size: " <<  avg_size_per_packet
+                                     << "\t Avg Pkt Size: " << avg_size_per_packet
                                      << "\t Interval Duration: " << interval_duration
-                                     << "\t Packets Per Second: " << 1/interval_duration << "\n");
+                                     << "\t Packets Per Second: " << 1 / interval_duration << "\n");
 
   rate_send_app->SetAttribute("MaxBytes", UintegerValue(max_size));
   rate_send_app->SetAttribute("BytesPerInterval", UintegerValue(bytes_per_period));
@@ -136,11 +136,10 @@ InstallRateSend(Ptr<Node> srcHost, Ptr<Node> dstHost, uint16_t dport, uint32_t n
 
 
 void
-InstallNormalBulkSend(Ptr<Node> srcHost, Ptr<Node> dstHost, uint16_t dport, uint64_t size, double startTime)
-{
+InstallNormalBulkSend(Ptr<Node> srcHost, Ptr<Node> dstHost, uint16_t dport, uint64_t size, double startTime) {
 
   Ipv4Address addr = GetNodeIp(dstHost);
-  Address sinkAddress (InetSocketAddress (addr, dport));
+  Address sinkAddress(InetSocketAddress(addr, dport));
 
   Ptr<BulkSendApplication> bulkSender = CreateObject<BulkSendApplication>();
 
@@ -158,82 +157,81 @@ InstallNormalBulkSend(Ptr<Node> srcHost, Ptr<Node> dstHost, uint16_t dport, uint
 //  return socket;
 }
 
-std::unordered_map <std::string, std::vector<uint16_t>>
-InstallSinks(NodeContainer hosts, uint16_t sinksPerHost, uint32_t duration, std::string protocol)
-{
+std::unordered_map<std::string, std::vector<uint16_t>>
+InstallSinks(NodeContainer hosts, uint16_t sinksPerHost, uint32_t duration, std::string protocol) {
 
-	NS_ASSERT_MSG(sinksPerHost < ((uint16_t) -2), "Can not create such amount of sinks");
+  NS_ASSERT_MSG(sinksPerHost < ((uint16_t) -2), "Can not create such amount of sinks");
 
-    std::unordered_map <std::string, std::vector<uint16_t>> hostsToPorts;
-    Ptr<UniformRandomVariable> random_generator = CreateObject<UniformRandomVariable> ();
-    uint16_t starting_port;
+  std::unordered_map<std::string, std::vector<uint16_t>> hostsToPorts;
+  Ptr<UniformRandomVariable> random_generator = CreateObject<UniformRandomVariable>();
+  uint16_t starting_port;
 
-    for(NodeContainer::Iterator host = hosts.Begin(); host != hosts.End (); host ++){
+  for (NodeContainer::Iterator host = hosts.Begin(); host != hosts.End(); host++) {
 
-        starting_port = random_generator->GetInteger(0, (uint16_t)-2 - sinksPerHost);
-        std::string host_name = GetNodeName((*host));
+    starting_port = random_generator->GetInteger(0, (uint16_t) -2 - sinksPerHost);
+    std::string host_name = GetNodeName((*host));
 
-        hostsToPorts[host_name] = std::vector<uint16_t>();
+    hostsToPorts[host_name] = std::vector<uint16_t>();
 
-        for (int i = 0; i < sinksPerHost ; i++){
-            InstallSink((*host), starting_port+i, duration, protocol);
-            //Add port into the vector
-            //NS_LOG_DEBUG("Install Sink: " << host_name << " port:" << starting_port+i);
-            hostsToPorts[host_name].push_back(starting_port+i);
-        }
+    for (int i = 0; i < sinksPerHost; i++) {
+      InstallSink((*host), starting_port + i, duration, protocol);
+      //Add port into the vector
+      //NS_LOG_DEBUG("Install Sink: " << host_name << " port:" << starting_port+i);
+      hostsToPorts[host_name].push_back(starting_port + i);
+    }
 
-      }
+  }
 
   return hostsToPorts;
 }
 
 //DO THE SAME WITH THE BULK APP, WHICH IS PROBABLY WHAT WE WANT TO HAVE.
-void InstallBulkSend(Ptr <Node> srcHost, Ptr <Node> dstHost, uint16_t dport, uint64_t size, double startTime,
-                     Ptr <OutputStreamWrapper> fctFile, Ptr <OutputStreamWrapper> counterFile,
-                     Ptr <OutputStreamWrapper> flowsFile,
+void InstallBulkSend(Ptr<Node> srcHost, Ptr<Node> dstHost, uint16_t dport, uint64_t size, double startTime,
+                     Ptr<OutputStreamWrapper> fctFile, Ptr<OutputStreamWrapper> counterFile,
+                     Ptr<OutputStreamWrapper> flowsFile,
                      uint64_t flowId, uint64_t *recordedFlowsCounter, double *startRecordingTime,
                      double recordingTime) {
 
-    Ipv4Address addr = GetNodeIp(dstHost);
-    Address sinkAddress(InetSocketAddress(addr, dport));
+  Ipv4Address addr = GetNodeIp(dstHost);
+  Address sinkAddress(InetSocketAddress(addr, dport));
 
-    Ptr <CustomBulkApplication> bulkSender = CreateObject<CustomBulkApplication>();
+  Ptr<CustomBulkApplication> bulkSender = CreateObject<CustomBulkApplication>();
 
 //  Ptr<Socket> socket;
 //  socket = Socket::CreateSocket (srcHost, TcpSocketFactory::GetTypeId ());
 //  bulkSender->SetSocket(socket);
 
-    bulkSender->SetAttribute("Protocol", TypeIdValue(TcpSocketFactory::GetTypeId()));
-    bulkSender->SetAttribute("MaxBytes", UintegerValue(size));
-    bulkSender->SetAttribute("Remote", AddressValue(sinkAddress));
-    bulkSender->SetAttribute("FlowId", UintegerValue(flowId));
+  bulkSender->SetAttribute("Protocol", TypeIdValue(TcpSocketFactory::GetTypeId()));
+  bulkSender->SetAttribute("MaxBytes", UintegerValue(size));
+  bulkSender->SetAttribute("Remote", AddressValue(sinkAddress));
+  bulkSender->SetAttribute("FlowId", UintegerValue(flowId));
 
-    bulkSender->SetOutputFile(fctFile);
-    bulkSender->SetCounterFile(counterFile);
-    bulkSender->SetFlowsFile(flowsFile);
+  bulkSender->SetOutputFile(fctFile);
+  bulkSender->SetCounterFile(counterFile);
+  bulkSender->SetFlowsFile(flowsFile);
 
-    bulkSender->SetStartRecordingTime(startRecordingTime);
-    bulkSender->SetRecordingTime(recordingTime);
-    bulkSender->SetRecordedFlowsCounter(recordedFlowsCounter);
+  bulkSender->SetStartRecordingTime(startRecordingTime);
+  bulkSender->SetRecordingTime(recordingTime);
+  bulkSender->SetRecordedFlowsCounter(recordedFlowsCounter);
 
-    //Install app
-    srcHost->AddApplication(bulkSender);
+  //Install app
+  srcHost->AddApplication(bulkSender);
 
-    bulkSender->SetStartTime(Seconds(startTime));
-    bulkSender->SetStopTime(Seconds(1000));
+  bulkSender->SetStartTime(Seconds(startTime));
+  bulkSender->SetStopTime(Seconds(1000));
 
-    return;
+  return;
 }
 
 void
-SendBindTest(Ptr<Node> src, NodeContainer receivers, std::unordered_map<std::string, std::vector<uint16_t>> hostsToPorts, uint32_t flows)
-{
-	for (uint32_t i = 0 ; i < flows ;i++){
-		Ptr<Node> dst  = receivers.Get(random_variable->GetInteger(0, receivers.GetN()-1));
-		std::vector<uint16_t> availablePorts = hostsToPorts[GetNodeName(dst)];
-		uint16_t dport = RandomFromVector<uint16_t>(availablePorts);
-		InstallNormalBulkSend(src, dst, dport, 50000, random_variable->GetValue(1, 10));
-	}
+SendBindTest(Ptr<Node> src, NodeContainer receivers,
+             std::unordered_map<std::string, std::vector<uint16_t>> hostsToPorts, uint32_t flows) {
+  for (uint32_t i = 0; i < flows; i++) {
+    Ptr<Node> dst = receivers.Get(random_variable->GetInteger(0, receivers.GetN() - 1));
+    std::vector<uint16_t> availablePorts = hostsToPorts[GetNodeName(dst)];
+    uint16_t dport = RandomFromVector<uint16_t>(availablePorts);
+    InstallNormalBulkSend(src, dst, dport, 50000, random_variable->GetValue(1, 10));
+  }
 }
 
 
--- scratch/flow-model-test.cc
+++ scratch/flow-model-test.cc
@@ -17,52 +17,74 @@ NS_LOG_COMPONENT_DEFINE ("flow-model-test");
 
 
 int
-main (int argc, char *argv[])
-{
-    CommandLine cmd;
-    cmd.Parse (argc, argv);
+main(int argc, char *argv[]) {
+  CommandLine cmd;
+  cmd.Parse(argc, argv);
 
-    NodeContainer nodes;
-    nodes.Create (2);
+  NodeContainer nodes;
+  nodes.Create(2);
 
-    PointToPointHelper pointToPoint;
-    pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Gbps"));
-    pointToPoint.SetChannelAttribute ("Delay", StringValue ("150ms"));
+  LogComponentEnable("traffic-generation", LOG_DEBUG);
 
-    Config::SetDefault("ns3::TcpSocket::DataRetries", UintegerValue(5)); //retranmissions
-    Config::SetDefault("ns3::TcpSocketBase::MinRto", TimeValue(MilliSeconds(200))); //min RTO value that can be set
-    Config::SetDefault("ns3::TcpSocketBase::Sack", BooleanValue(true)); //enable sack
+  PointToPointHelper pointToPoint;
+  pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Gbps"));
+  pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
+
+  //Tcp Socket (general socket conf)
+  Config::SetDefault("ns3::TcpSocket::SndBufSize", UintegerValue(4000000));
+  Config::SetDefault("ns3::TcpSocket::RcvBufSize", UintegerValue(4000000));
+  Config::SetDefault("ns3::TcpSocket::SegmentSize", UintegerValue(1446)); //MTU 1446
+  Config::SetDefault("ns3::TcpSocket::InitialSlowStartThreshold", UintegerValue(4294967295));
+  Config::SetDefault("ns3::TcpSocket::InitialCwnd", UintegerValue(1));
+
+  //Can be much slower than my rtt because packet size of syn is 60bytes
+  Config::SetDefault("ns3::TcpSocket::ConnCount", UintegerValue(5)); //retrnamissions during connection
+  Config::SetDefault("ns3::TcpSocket::DataRetries", UintegerValue(15)); //retranmissions
+  Config::SetDefault("ns3::TcpSocket::DelAckTimeout", TimeValue(MilliSeconds(150 / 50)));
+// 	Config::SetDefault ("ns3::TcpSocket::DelAckCount", UintegerValue(2));
+  Config::SetDefault("ns3::TcpSocket::TcpNoDelay", BooleanValue(true)); //disable nagle's algorithm
+  Config::SetDefault("ns3::TcpSocket::PersistTimeout",
+                     TimeValue(NanoSeconds(6000000000))); //persist timeout to porbe for rx window
+
+  //Tcp Socket Base: provides connection orientation, sliding window, flow control; congestion control is delegated to the subclasses (i.e new reno)
+
+  Config::SetDefault("ns3::TcpSocketBase::MaxSegLifetime", DoubleValue(10));
+  Config::SetDefault("ns3::TcpSocketBase::MinRto", TimeValue(MilliSeconds(200))); //min RTO value that can be set
+  Config::SetDefault("ns3::TcpSocketBase::ClockGranularity", TimeValue(MicroSeconds(1)));
+  Config::SetDefault("ns3::TcpSocketBase::ReTxThreshold", UintegerValue(3)); //same than DupAckThreshold
+
+  Config::SetDefault("ns3::TcpSocketBase::Sack", BooleanValue(true)); //enable sack
+  Config::SetDefault("ns3::TcpSocketBase::LimitedTransmit", BooleanValue(true)); //enable sack
+
+  NetDeviceContainer link1;
+  link1 = pointToPoint.Install(NodeContainer(nodes.Get(0), nodes.Get(1)));
+
+  InternetStackHelper stack;
+  stack.Install(nodes);
+
+  Ipv4AddressHelper address;
+  address.SetBase("10.1.1.0", "255.255.255.252");
+  Ipv4InterfaceContainer interfaces = address.Assign(link1);
+
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables();
+
+  Packet::EnablePrinting();
+
+  //traffic
+  uint16_t dst_port = 80;
+  InstallSink(nodes.Get(1), dst_port, 0, "TCP");
+
+  InstallRateSend(nodes.Get(0), nodes.Get(1), dst_port, 8, 1500, 0.342, 1);
+
+  //InstallNormalBulkSend(nodes.Get(0), nodes.Get(2), dst_port, 1500000, 1);
+  //InstallNormalBulkSend(nodes.Get(0), nodes.Get(2), dst_port, 1500000, 1);
+
+  pointToPoint.EnablePcap("../../test_duration/out", link1.Get(0), bool(1));
 
 
-    NetDeviceContainer link1;
-    link1 = pointToPoint.Install (NodeContainer(nodes.Get(0), nodes.Get(1)));
+  //Simulator::Stop (Seconds (100000));
+  Simulator::Run();
+  Simulator::Destroy();
 
-    InternetStackHelper stack;
-    stack.Install (nodes);
-
-    Ipv4AddressHelper address;
-    address.SetBase ("10.1.1.0", "255.255.255.252");
-    Ipv4InterfaceContainer interfaces = address.Assign (link1);
-
-    Ipv4GlobalRoutingHelper::PopulateRoutingTables();
-
-    Packet::EnablePrinting();
-
-    //traffic
-    uint16_t dst_port = 7001;
-    InstallSink(nodes.Get(1), dst_port, 0, "TCP");
-
-    InstallRateSend(nodes.Get(0),nodes.Get(1),dst_port,5,1545,0.342, 1);
-
-    //InstallNormalBulkSend(nodes.Get(0), nodes.Get(2), dst_port, 1500000, 1);
-    //InstallNormalBulkSend(nodes.Get(0), nodes.Get(2), dst_port, 1500000, 1);
-
-    pointToPoint.EnablePcap("out", link1.Get(0), bool(1));
-
-
-    //Simulator::Stop (Seconds (100000));
-    Simulator::Run ();
-    Simulator::Destroy ();
-
-    return 0;
+  return 0;
 }
